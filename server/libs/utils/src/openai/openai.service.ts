778c2fd89df196fe724f83449f430bc274cc4dac
kkywalk2 <kkywalk2@gmail.com>
2024-06-23 23:01:02 +0900

    feat: openai 요청 함수 구현

--- server/libs/utils/src/openai/openai.service.ts
+++ server/libs/utils/src/openai/openai.service.ts
@@ -4,12 +4,21 @@ import OpenAI from "openai";
 @Injectable()
 export class OpenAIService {
 
-    openai = new OpenAI();
+    openai = new OpenAI({
+        apiKey: process.env.OPEN_AI_API_KEY,
+    });
 
-    async getChatCompletions() {
+    async getChatCompletions(text: string): Promise<string> {
         const completion = await this.openai.chat.completions.create({
-            messages: [{ role: "system", content: "You are a helpful assistant." }],
             model: "gpt-3.5-turbo",
+            messages: [
+                { role: "system", content: "You are a helpful assistant that summarizes web pages." },
+                { role: "user", content: `${text}` }
+            ],
+            max_tokens: 300,
+            temperature: 0.7,
         });
+
+        return completion.choices[0].message.content;
     }
 }
